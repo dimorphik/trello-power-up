fb94c447a9bf4d07ebc4b39cea0eb2bf561470ad
Robin Gabrielli <dimorphik@gmail.com>
2022-09-30 12:21:15 -0700

    testing card stuff

--- src/js/connector.js
+++ src/js/connector.js
@@ -5,7 +5,6 @@ window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     //    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
     return t.card("all").then(function (card) {
-      console.log(card);
       return [
         {
           text: card.idShort,
@@ -18,10 +17,14 @@ window.TrelloPowerUp.initialize({
     // you can look through them and 'claim' any that you want to
     // include in your section.
 
+    const cardId = t.card("all").then((card) => card.idShort);
+
     // we will just claim urls for Yellowstone
     var claimed = options.entries.filter(function (attachment) {
       return attachment.url.indexOf("http://www.nps.gov/yell/") === 0;
     });
+
+    console.log(`attachment claimed for card ${cardId}? ${claimed}`);
 
     // you can have more than one attachment section on a card
     // you can group items together into one section, have a section
